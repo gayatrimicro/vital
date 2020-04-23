ce3281fb69a70beaf59ec8db8c5445a3585d0e64
Rits-theDeveloper <ritesh.mestry.naresh@gmail.com>
2020-04-23 14:50:37 +0530

    Email change

--- request.php
+++ request.php
@@ -11,7 +11,7 @@
 	 
 	 // $location = $_POST["location"];
 
-	 $subject ="Callback Request by " . $firstname;
+	$subject ="Callback Request by " . $firstname;
 	$headers  = 'MIME-Version: 1.0' . "\r\n";
 	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 	$headers .= 'From: <' . $email .">" ."\r\n";
